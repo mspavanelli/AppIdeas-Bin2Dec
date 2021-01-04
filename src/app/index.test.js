cc3bdf6324c61ddf3e19bde704bd25baf68715aa
Matheus Pavanelli <mspavanelli@gmail.com>
2021-01-04 02:05:58 -0300

    :white_check_mark: test: add binary to decimal function test
    tests
    
    -  input:
    	- must have only 0s and 1s;
    	- must be a string;
    	- must not be empty
    -  output: if converts properly;

---
+++ src/app/index.test.js
@@ -0,0 +1,32 @@
+const { convertBinaryToDecimal } = require("./index");
+
+describe("Binary to decimal", () => {
+  it("should convert correctly", () => {
+    expect(convertBinaryToDecimal("0")).toBe(0);
+    expect(convertBinaryToDecimal("1")).toBe(1);
+    expect(convertBinaryToDecimal("10")).toBe(2);
+    expect(convertBinaryToDecimal("11")).toBe(3);
+    expect(convertBinaryToDecimal("11111111")).toBe(255);
+  });
+
+  it("should not be empty", () => {
+    expect(() => convertBinaryToDecimal("")).toThrow(
+      new Error("Binary number must not be empty")
+    );
+  });
+
+  it("should accept only strings", () => {
+    expect(() => convertBinaryToDecimal(0)).toThrow(
+      new Error("Binary number must be a string")
+    );
+  });
+
+  it("should accept only 0s and 1", () => {
+    expect(() => convertBinaryToDecimal("2")).toThrow(
+      new Error("Binary number must contain only 0s and 1s")
+    );
+    expect(() => convertBinaryToDecimal("a")).toThrow(
+      new Error("Binary number must contain only 0s and 1s")
+    );
+  });
+});
